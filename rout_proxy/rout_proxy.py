0547dbe337e9aef2ccc921c67d1da7fa7a6f938e
SchlexanderOlz <Ihavenonameforthischannel@gmail.com>
2023-02-16 21:31:32 +0000

    added a blacklist and implemanting unallowed page

--- rout_proxy/rout_proxy.py
+++ rout_proxy/rout_proxy.py
@@ -1,6 +1,7 @@
 import socket
 from _thread import *
 import ssl
+import json
 
 
 class ProxyServer:
@@ -10,25 +11,25 @@ class ProxyServer:
         self.NETWORK = "0.0.0.0"
         self.BUFFERSIZE = 8124
 
-    def start(self):
+    def start(self): # --> Used to start the proxy server
         
         suscket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         suscket.bind((self.NETWORK, self.PORT))
-        suscket.listen(5)
+        suscket.listen(5) # --> Proxy is listening for max 5 clients
         
         print("[*] Proxy is running on {} and Port: {}".format(self.NETWORK, self.PORT))
         
         while True:
-            connection, address = suscket.accept()
-            data = connection.recv(self.BUFFERSIZE)
+            client, address = suscket.accept()
+            data = client.recv(self.BUFFERSIZE)
             print("[*] New connection from {}".format(address[0]))
 
-            start_new_thread(self.get_request_data, (connection, data))
+            start_new_thread(self.get_request_data, (client, data)) # --> Starting new thread to get the request data
     
-    def get_request_data(self, connection, data:bytes):
-        data_dec = data.decode().split("\r")
-        temp = data_dec[1].split(" ")[1].split(":")
-
+    def get_request_data(self, client:socket.socket, data:bytes):
+        data_dec = data.decode().split("\r") # --> Seperating the request headers
+        temp = data_dec[1].split(" ")[1].split(":") # --> Taking the second header (Where the host's address is loacated),
+                                                          #splitting the "Host: " String from it and seperating the Hostaddr from the port
 
         port = 80
         if len(temp) > 1:
@@ -40,30 +41,33 @@ class ProxyServer:
 
         if self.is_allowed(server):
             print("[*] Allowing request to {} on port: {}".format(server, port))
-            start_new_thread(self.send_request_server, (server, port, data, connection))
+            start_new_thread(self.send_request_server, (server, port, data, client))
+        else:
+            print("[*] Blocked request to {} on port: {}".format(server, port))
+            start_new_thread(self.load_block_info, (server, client))
         
     def send_request_server(self, server, port, data, client:socket.socket):
         
-        if port == 443:
+        if port == 443: # --> If the request is an https request a secure ssl connection is established
             ctxt = ssl.create_default_context()
             ctxt.verify_mode = ssl.CERT_REQUIRED
-            ctxt.load_verify_locations(cafile="/etc/self-signed-certs/ca.pem")
+            ctxt.load_verify_locations(cafile="/etc/self-signed-certs/ca.pem") # --> Wrong file at the moment
             server_sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
             server_sock = ctxt.wrap_socket(server_sock, server_hostname=server)
         else:
-            server_sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+            server_sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM) # --> Host we want to get data from
             
         try:
             server_sock.connect((server, port))
             server_sock.sendall(data)
-        except socket.gaierror as e:
+        except (socket.gaierror, ConnectionRefusedError) as e: # Haha a gay-error
             print(e)
             print("[*] Can't connect to Host")
         
         response = b''
         while True:
             reply = server_sock.recv(self.BUFFERSIZE)
-            if len(reply) == 0:
+            if len(reply) == 0: # --> When the server stops sending data
                 break
             response += reply
         
@@ -74,8 +78,19 @@ class ProxyServer:
         client.close()
         
     def is_allowed(self, server):
-        return True
+        with open("black_list.json") as json_data:
+            data = json.load(json_data)
+            return not server in data["hosts"]["address"]
         
-        
+    def load_block_info(self, server, client:socket.socket):
+        with open("unnallowed_page/unnallowed.html", "rb") as html_data:
+            html_text = html_data.read()
+            resp_len = len(html_text).to_bytes(2, byteorder='big')
+            response = b'HTTP/1.1 403 Forbidden\r\nContent-Type: text/html\r\nContent-Length: ' + resp_len + b'\r\n\r\n' + html_text
+            print(response)
+        client.sendall(response)
+        client.close()
+
+     
 if __name__ == "__main__":
     ProxyServer().start()
